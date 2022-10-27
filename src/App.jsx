3e07568a6b737a2e62b4cb12b2960800b0144da7
legcy <legcy143@gamil.com>
2022-10-27 20:00:38 +0530

    landing page and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import React , {Fragment} from 'react'
 import Navbar from './components/navs/Navbar'
 import style from "./styles/home.module.css"
 
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 const App = () => {
   return (
-    <Fragment>
+      <main className={style.main_page}>
       <Navbar/>
       <main className={style.home_container}>
         <section className={style.home_getstarted}>
-          <h1>Api's that makes you productive</h1>
+          <h1>Api's that makes you <span>productive</span></h1>
           <h2>Forget paid api's</h2>
           <p>we provide free api service</p>
             <h3>get started and browse free api</h3>
-          <div className={style.home_btns}>
-            <button>read docs</button>
-            <button>get start</button>
-          </div>
+            <button>get started <ArrowForwardIcon/></button>
         </section>
-        <img src="/vite.svg" alt="" />
+        <img className={style.landing_img} src="/landing_ui.gif" alt="img" />
       </main>
-    </Fragment>
+      </main>
   )
 }
 
--- src/components/navs/Navbar.jsx
+++ src/components/navs/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import style from "./nav.module.css"
+import ClearAllIcon from '@mui/icons-material/ClearAll'; 
+import SearchIcon from '@mui/icons-material/Search';
+// for menu icon (ClearAllIcon)
 import { Link , useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
@@ -7,15 +10,16 @@ const Navbar = () => {
   return (
     <nav className={style.main_nav}>
       <section className={style.logo}>
-        <h1>logo</h1>
+        <img src="/xapiy_logo.png" alt="img" />
       </section>
       <section className={style.nav_search}>
-        <input type="text" placeholder='search api'/>
+      <SearchIcon />
+        <input type="search" placeholder='search api'/>
       </section>
       <section className={style.links_and_more}>
         <Link to="/browse">browse</Link>
         <Link to="/editor">test api</Link>
-        <button onClick={()=>{navigate("/")}}>signup</button>
+        <button onClick={()=>{navigate("/")}}>sign up</button>
       </section>
     </nav>
   )
